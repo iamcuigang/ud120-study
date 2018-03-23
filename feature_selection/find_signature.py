c34e49698208733ad964d2c4b306ecca1bffb927
iamcuigang <iamcuigang@yeah.net>
2018-03-23 15:33:02 +0800

    something

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -40,6 +40,14 @@ labels_train   = labels_train[:150]
 
 
 ### your code goes here
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
+# print len(features_train[0])
+clf = tree.DecisionTreeClassifier()#min_samples_split=40)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+acc = accuracy_score(pred, labels_test)
+print acc
 
 
